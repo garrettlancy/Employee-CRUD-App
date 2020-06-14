671d46f5a7eb51273b6a6dd601cf2526aedda141
garrettlancy <glancy@live.com>
2020-06-13 21:34:21 -0400

    Second Commit

--- php/employees.php
+++ php/employees.php
@@ -1,28 +1,28 @@
 <?php
 // Include config file
-                    require_once "config.php";
+require_once "config.php";
 
-                    // Attempt select query execution
-                    $sql = "SELECT * FROM employees";
-                    if($result = mysqli_query($link, $sql)){
-                        if(mysqli_num_rows($result) > 0){
-                while($row = mysqli_fetch_array($result)){
+// Attempt select query execution
+$sql = "SELECT * FROM employees";
+if ($result = mysqli_query($link, $sql)) {
+    if (mysqli_num_rows($result) > 0) {
+        while ($row = mysqli_fetch_array($result)) {
 
-                    $id = $row['id'];
-                    $name = $row['name'];
-                    $address = $row['address'];
-                    $salary = $row['salary'];
-                    $result[] = array('id'=>$id, 'name'=>$name, 'address'=>$address, 'salary'=>$salary);
-                }
-                $json = array('status' => 1, 'info' => $result);
-                // Free result set
-                mysqli_free_result($result);
-                } else{
-                $json = array('status' => 0, 'message' => 'No records were found.');
-                }
-                } else{
-                echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
-                }
+            $id = $row['id'];
+            $name = $row['name'];
+            $address = $row['address'];
+            $salary = $row['salary'];
+            $result[] = array('id' => $id, 'name' => $name, 'address' => $address, 'salary' => $salary);
+        }
+        $json = array('status' => 1, 'info' => $result);
+        // Free result set
+        mysqli_free_result($result);
+    } else {
+        $json = array('status' => 0, 'message' => 'No records were found.');
+    }
+} else {
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
 
-                // Close connection
-                mysqli_close($link);
+// Close connection
+mysqli_close($link);
