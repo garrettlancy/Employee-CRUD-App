a9a5b4d2332a50f7215687f4eebb51a8ae6366e0
garrettlancy <glancy@live.com>
2020-06-15 13:05:21 -0400

    Modified database config file for Heroku database

--- php/config.php
+++ php/config.php
@@ -1,8 +1,13 @@
 <?php
 
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'root');
+//define('DB_SERVER', 'localhost');
+//define('DB_USERNAME', 'root');
+//define('DB_PASSWORD', 'root');
+//define('DB_NAME', 'employee_db');
+
+define('DB_SERVER', 'us-cdbr-east-05.cleardb.net');
+define('DB_USERNAME', 'b8ca4a4bfc8fb5');
+define('DB_PASSWORD', '749b6271');
 define('DB_NAME', 'employee_db');
 
 /* Attempt to connect to MySQL database */
